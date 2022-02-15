c9d1d1b4948d1860af3aab7215308da1bafb2bce
mkjanke <mkjanke@gmail.com>
2022-02-15 14:16:36 -0600

    Update comment in settings-dist.h

--- include/settings-dist.h
+++ include/settings-dist.h
@@ -15,9 +15,9 @@
 #define TXD2 17
 
 // BlueTooth UUID's
-#define SERVICE_UUID        "611f9238-3915-11ec-8d3d-0242ac130003"  // 9238
-#define MESSAGE_UUID        "6bcbec08-7fbb-11ec-a8a3-0242ac120002"  // 0002 String - Nextion Command
-#define EVENT_UUID          "6bcbee60-7fbb-11ec-a8a3-0242ac120002"
-#define UPTIME_UUID         "611f96f2-3915-11ec-8d3d-0242ac130003"  // 96f2 String
+#define SERVICE_UUID        "611f9238-3915-11ec-8d3d-0242ac130003"  // Bluetooth service
+#define MESSAGE_UUID        "6bcbec08-7fbb-11ec-a8a3-0242ac120002"  // Nextion Command (write to this characteristic to send command to Nextion)
+#define EVENT_UUID          "6bcbee60-7fbb-11ec-a8a3-0242ac120002"  // Nextion message
+#define UPTIME_UUID         "611f96f2-3915-11ec-8d3d-0242ac130003"  // ESP32 Uptime
 
 #endif //SETTINS_H
